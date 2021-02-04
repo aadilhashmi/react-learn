5ea10c1f7f36f2116ad00436c37bb25f79c53975
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-04 15:25:32 +0530

    huhk

--- src/Components/MainBody/PropsWithClassComponent/PropsWithClassParent.jsx
+++ src/Components/MainBody/PropsWithClassComponent/PropsWithClassParent.jsx
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import PropsWithClassChild from "./PropsWithClassChild";
 
 class PropsWithClassParent extends Component {
+  state = {
+    person: [
+      { name: "thor", age: 25 },
+      { name: "Superman", age: 25 },
+      { name: "wonderwomen", age: 30 },
+    ],
+  };
+
   render() {
     return (
       <div>
         <PropsWithClassChild name="aamer khan" age="56" />
         <PropsWithClassChild name="salman khan" age="56" />
-        <PropsWithClassChild name="shahruk khan khan" age="56" />
+            <PropsWithClassChild name="shahruk khan khan" age="56" />
+            <PropsWithClassChild    name={this.state.}>
       </div>
     );
   }
