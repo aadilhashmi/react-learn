62990385dae00097b6b7e18d6aa2a54b3a3972fb
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-04 15:59:17 +0530

    react function  and classes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-// import { ClassMates } from "./Components/ClassMates/ClassMates";
+import { ClassMates } from "./Components/ClassMates/ClassMates";
 import { Parent } from "./Components/MainBody/PropsWithfunction/Parent";
 import ParentClasses from "./Components/MainBody/PropsWithClassComponent/ParentClasses";
 import PropsWithClassParent from "./Components/MainBody/PropsWithClassComponent/PropsWithClassParent";
@@ -9,9 +9,9 @@ function App() {
   return (
     <React.Fragment>
       <div className="App">
-        {/* <ClassMates /> */}
-        {/* <Parent /> */}
-        {/* <ParentClasses /> */}
+        <ClassMates />
+        <Parent />
+        <ParentClasses />
         <PropsWithClassParent />
       </div>
     </React.Fragment>
