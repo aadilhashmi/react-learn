861737a56e5c3a7354721c9adc9286e067bd6fe4
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-05 16:28:05 +0530

    hey

--- src/Components/HomePage/Landing.jsx
+++ src/Components/HomePage/Landing.jsx
@@ -7,8 +7,6 @@ export const Landing = () => {
     <React.Fragment>
       <ClassMates />
       <PropsWithClassParent />
-      {/* <HeaderSection />
-      <RightNavigation /> */}
     </React.Fragment>
   );
 };
--- src/Components/MainBody/PropsWithClassComponent/PropsWithClassParent.jsx
+++ src/Components/MainBody/PropsWithClassComponent/PropsWithClassParent.jsx
@@ -9,6 +9,24 @@ export class PropsWithClassParent extends Component {
       { name: "wonderwomen", age: 30 },
     ],
   };
+  changNameHandler = () => {
+    this.setState({
+      person: [
+        { name: "AADIL", age: 10 },
+        { name: "RIZWAN", age: 20 },
+        { name: "TAUSEEF", age: 34 },
+      ],
+    });
+  };
+  changNameHandler1 = () => {
+    this.setState({
+      person: [
+        { name: "khawaja", age: 10 },
+        { name: "aamer", age: 20 },
+        { name: "salman", age: 34 },
+      ],
+    });
+  };
 
   render() {
     return (
@@ -25,6 +43,9 @@ export class PropsWithClassParent extends Component {
           name={this.state.person[2].name}
           age={this.state.person[2].age}
         />
+        <button onClick={this.changNameHandler}> ON CLICK EVENT</button>
+        <br />
+        <button onClick={this.changNameHandler1}> ON CLICK EVENT</button>
       </div>
     );
   }
