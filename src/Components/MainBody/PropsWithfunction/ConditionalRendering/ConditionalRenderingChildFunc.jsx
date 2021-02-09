af76da997e76df2e354b904698b10b8829fb9951
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-09 15:21:17 +0530

    hiiii

---
+++ src/Components/MainBody/PropsWithfunction/ConditionalRendering/ConditionalRenderingChildFunc.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ConditionalRenderingChildFunc = () => {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+};
+
+export default ConditionalRenderingChildFunc;
