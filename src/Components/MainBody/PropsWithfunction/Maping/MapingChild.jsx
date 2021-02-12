ff57f01135f798438cb817d0c04fa002112f157c
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-12 15:58:29 +0530

    hiiii

--- src/Components/MainBody/PropsWithfunction/Maping/MapingChild.jsx
+++ src/Components/MainBody/PropsWithfunction/Maping/MapingChild.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const MapingChild = () => {
-  return <div></div>;
+const MapingChild = (item) => {
+  return (
+    <div>
+      <p>
+        My name {item.first} and my age {item.firstage}
+      </p>
+    </div>
+  );
 };
 
 export default MapingChild;
