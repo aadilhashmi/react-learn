d26fe251e0797af79304c2ebf9c4c1c6165745eb
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-13 17:16:11 +0530

    hiiiii

--- src/Components/MainBody/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
+++ src/Components/MainBody/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const FetchingDataFromJsonChild = ({ item }) => {
+const FetchingDataFromJsonChild = ({ Data }) => {
   return (
     <div>
       <Card style={{ width: "25rem" }}>
-        <Card.Img variant="top" src="img.jpg" width="300px" />
+        <Card.Img variant="top" src={Data.image} width="300px" />
         <Card.Body>
-          {item.Data}
-          <Card.Title></Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Title>{Data.name}</Card.Title>
+          <Card.Text>{Data.discription}</Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
