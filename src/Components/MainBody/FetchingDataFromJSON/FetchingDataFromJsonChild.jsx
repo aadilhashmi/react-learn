f2626b44cd0b77188071d8b984360e2361196f19
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-13 18:16:28 +0530

    hiiiii

--- src/Components/MainBody/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
+++ src/Components/MainBody/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./FetchingData.css";
 import { Card, Button } from "react-bootstrap";
 
 const FetchingDataFromJsonChild = ({ Data }) => {
   return (
-    <div>
-      <Card style={{ width: "25rem" }}>
+    <div className="main">
+      <Card className="card" style={{ width: "25rem" }}>
         <Card.Img variant="top" src={Data.image} width="300px" />
         <Card.Body>
           <Card.Title>{Data.name}</Card.Title>
