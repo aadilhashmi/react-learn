2fc0100b8945c26814587cc968b96176b1923ff4
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-13 15:36:22 +0530

    hiiii

---
+++ src/Components/State/Reducers/PersonReducer.js
@@ -0,0 +1,79 @@
+const PersonReducer = {
+  person: [
+    {
+      name: "AMJAD",
+      age: 34,
+      discription: "he is a final year student  from mgm engineering collage",
+    },
+    {
+      name: "zubair",
+      age: 34,
+      discription: "he is a final year student  from mgm engineering collage",
+    },
+    {
+      name: "Aamer",
+      age: 34,
+      discription: "he is a second year student  from mgm engineering collage",
+    },
+    {
+      name: "khawaja",
+      age: 34,
+      discription: "he is a third year student  from mgm engineering collage",
+    },
+    {
+      name: "salman",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "umair",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "rizwan",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "zubair",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "tausif",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "sam",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "mazhar",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "aadil",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "shoib",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "rehan",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+    {
+      name: "rabey",
+      age: 34,
+      discription: "he is a first year student  from mgm engineering collage",
+    },
+  ],
+};
