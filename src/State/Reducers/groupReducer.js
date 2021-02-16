9c208577a883463017f9613fafd8a65d39fb0215
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-16 15:53:26 +0530

    hiiiiiiiiiii

--- src/State/Reducers/groupReducer.js
+++ src/State/Reducers/groupReducer.js
@@ -1,1 +1,11 @@
 const groupReducer = () => {};
+
+const groupsDefaultState = {
+  groups: [
+    {
+      name: "Redux Practicle",
+      image: "img.jpg",
+      desc: "This first Group Created With Redux",
+    },
+  ],
+};
