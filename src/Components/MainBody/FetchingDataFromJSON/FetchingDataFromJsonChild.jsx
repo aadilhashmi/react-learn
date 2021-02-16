f95569fb7b58d7051370dabfb5a555fb2c569468
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-16 19:33:14 +0530

    hiiiiiiiiii

--- src/Components/MainBody/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
+++ src/Components/MainBody/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
@@ -6,12 +6,14 @@ const FetchingDataFromJsonChild = ({ Data }) => {
   return (
     <div className="main">
       <Card className="card" style={{ width: "20rem" }}>
-        <Card.Img variant="top" src={Data.image} width="200px" height="200px" />
+        <Card.Img className="img" variant="top" src={Data.image} />
         <Card.Body>
-          <Card.Title>{Data.name}</Card.Title>
+          <Card.Title className="title">{Data.name}</Card.Title>
           {Data.age}
-          <Card.Text>{Data.discription}</Card.Text>
-          <Button variant="primary">View More</Button>
+          <Card.Text className="desc">{Data.discription}</Card.Text>
+          <Button className="buttons" variant="primary">
+            View More
+          </Button>
         </Card.Body>
       </Card>
     </div>
