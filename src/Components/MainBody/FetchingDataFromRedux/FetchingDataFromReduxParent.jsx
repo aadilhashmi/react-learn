612287b5c099708934289cd36de848fc7e444a57
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-20 16:10:55 +0530

    hiiiiiiiiii

--- src/Components/MainBody/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
+++ src/Components/MainBody/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FetchingDataFromReduxChild from "./FetchingDataFromReduxChild";
+import { useSelector } from "react-redux";
+import { getAllGroups } from "../../../State/selectors/groupSelector";
 
 const FetchingDataFromReduxParent = () => {
-  return <div></div>;
+  const selector = useSelector(getAllGroups);
+  const [groupData, setGroupData] = useState();
+  useEffect(() => {
+    setGroupData(selector);
+  }, [selector]);
+  return (
+    <div>
+      {groupData &&
+        groupData.map((fetchData) => {
+          return (
+            <FetchingDataFromReduxChild Data={fetchData} key={fetchData.id} />
+          );
+        })}
+    </div>
+  );
 };
 
 export default FetchingDataFromReduxParent;
