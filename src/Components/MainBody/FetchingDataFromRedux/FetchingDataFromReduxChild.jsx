7326e6618cee09f50b437de7c2fb21be35fe15a9
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-20 16:48:20 +0530

    hiiiiiiii

--- src/Components/MainBody/FetchingDataFromRedux/FetchingDataFromReduxChild.jsx
+++ src/Components/MainBody/FetchingDataFromRedux/FetchingDataFromReduxChild.jsx
@@ -2,13 +2,19 @@ import React from "react";
 
 const FetchingDataFromReduxChild = ({ Data }) => {
   return (
-    <div style={{ display: "inline-block", padding: "20px" }}>
-      <h1>{Data.name}</h1>
+    <div
+      style={{
+        display: "inline-block",
+        padding: "30px",
+        border: "1px solid black",
+      }}
+    >
       <img
         style={{ width: "200px", height: "200px" }}
         src={Data.image}
         alt="Error"
       />
+      <h1>{Data.name}</h1>
     </div>
   );
 };
