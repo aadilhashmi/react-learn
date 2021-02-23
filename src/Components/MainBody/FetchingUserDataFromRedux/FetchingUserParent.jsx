65191325ae28c699210c457d2e4972eb090955d4
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-23 13:19:10 +0530

    hiiiiiii

--- src/Components/MainBody/FetchingUserDataFromRedux/FetchingUserParent.jsx
+++ src/Components/MainBody/FetchingUserDataFromRedux/FetchingUserParent.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getAllUser } from "../../../State/selectors/UserSelector";
 
 const FetchingUserParent = () => {
-  return <div></div>;
+  const selector = useSelector(getAllUser);
+  const [UserData, setUserData] = useState();
+  useEffect(() => {
+    setUserData(selector);
+  }, [selector]);
+    return <div>
+        {UserData &&
+            UserData.map((fetchData) => {
+                userData = (fetchData),
+              key=(fetchData.id)
+      }}
+  </div>;
 };
 
 export default FetchingUserParent;
