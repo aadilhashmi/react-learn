533917157f514759a431ee5581ae24f39f6da2a9
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-23 12:27:15 +0530

    HIIIIIIIII

---
+++ src/State/Action/UserReducer.js
@@ -0,0 +1,20 @@
+const userDefaultState = {
+  User: [
+    {
+      id: uuvidv4(),
+      name: "AADIL",
+      desc: "learning redux ",
+    },
+    // {
+    //   id: uuvidv4(),
+    //   name: "RIZWAN",
+    //   desc: "learning ANGULAR  ",
+    // },
+    // {
+    //   id: uuvidv4(),
+    //   name: "SALMAN",
+    //   desc: "learning REACTJS ",
+    // },
+  ],
+};
+export const 
