9c25167bf49c2afd25a6956a4f5937efade6c8ed
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-24 13:32:11 +0530

    hiiiiiii

--- src/Components/MainBody/PropsWithfunction/enable-disableButtonWithClassFunction/EnableDisable.jsx
+++ src/Components/MainBody/PropsWithfunction/enable-disableButtonWithClassFunction/EnableDisable.jsx
@@ -1,31 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class EnableDisable extends Component {
-  state = {
-    disable: true,
-  };
-  handleChange = (e) => {
-    if (e.target.value.lenght >= 6) {
-      this.setState({
-        disable: fasle,
-      });
-    } else {
-      this.state({
-        disable: true,
-      });
+export default class EnableDisable extends Component {
     }
-    render();
-    return (
-      <div>
-        <form>
-          <input type="text" placeholder="ENTER" onChange={this.handleChange} />
-          <br />
-          <br />
-          <button disabled={this.state.disable}>SUBMIT</button>
-        </form>
-      </div>
-    );
-  };
-}
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="ENTER"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <br />
+                    <button disabled={this.state.disabled}>SUBMIT</button>
+                </form>
+            </div>
 
-export default EnableDisable;
+        );
+    }
