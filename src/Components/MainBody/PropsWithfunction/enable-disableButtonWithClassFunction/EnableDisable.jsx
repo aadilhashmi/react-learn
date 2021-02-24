c35bf343ff4128dbe5f691705be0a6a41e260ae6
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-02-24 13:10:16 +0530

    hiiiiiiii

--- src/Components/MainBody/PropsWithfunction/enable-disableButtonWithClassFunction/EnableDisable.jsx
+++ src/Components/MainBody/PropsWithfunction/enable-disableButtonWithClassFunction/EnableDisable.jsx
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class EnableDisable extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="ENTER" onChange={this.handleChange} />
+          <br />
+          <br />
+          <button>SUBMIT</button>
+        </form>
+      </div>
+    );
   }
 }
 
