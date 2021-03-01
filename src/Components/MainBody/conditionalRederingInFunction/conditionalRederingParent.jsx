d065c7aca2bbe75f3afa5bd8605d8354c512024a
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-01 16:25:34 +0530

    conditional redering

---
+++ src/Components/MainBody/conditionalRederingInFunction/conditionalRederingParent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const conditionalRederingParent = () => {
+  return <div></div>;
+};
+
+export default conditionalRederingParent;
---
+++ src/Components/MainBody/conditionalRederingInFunction/conditionalRendringChild.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const conditionalRendringChild = () => {
+  return <div></div>;
+};
+
+export default conditionalRendringChild;
