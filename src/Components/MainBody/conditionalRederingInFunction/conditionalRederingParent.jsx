15b8f2643629dde2d133c67896c2857e717d09a8
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-02 19:06:44 +0530

    hiiiiiiiiiiiii

--- src/Components/MainBody/conditionalRederingInFunction/conditionalRederingParent.jsx
+++ src/Components/MainBody/conditionalRederingInFunction/conditionalRederingParent.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import ConditionalRendringChild from "./ConditionalRendringChild";
 
+const employe = [
+  {
+    name: "rizwan",
+    job: " php developer",
+  },
+  {
+    name: "rizwan 2",
+    job: " php developer",
+  },
+  {
+    name: "rizwan 3",
+    job: " php developer",
+  },
+];
 const ConditionalRederingParent = () => {
-  const [employData, setEmployeData] = useState({
-    employe: [
-      {
-        name: "rizwan",
-        job: " php developer",
-      },
-      {
-        name: "rizwan 2",
-        job: " php developer",
-      },
-      {
-        name: "rizwan 3",
-        job: " php developer",
-      },
-    ],
-  });
+  const [employData, setEmployeData] = useState(employe);
 
   const handleDelete = (index) => {
     const newEmployeData = [...employData];
     newEmployeData.splice(index, 1);
+    setEmployeData(newEmployeData);
   };
   return (
     <div>
       <button onClick={handleDelete}>DELETE</button>
-      {employData.employe.map((fetchData) => {
+      {employData.map((fetchData, index) => {
         return <ConditionalRendringChild passData={fetchData} />;
       })}
     </div>
