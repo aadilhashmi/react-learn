175327e75a84630397448d971411056033cb81da
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-03 12:34:59 +0530

    making use state

--- src/Components/MainBody/ConditionalRederingInFunction/A-ConditionalRenderingTernary/CondRendF_Parent.jsx
+++ src/Components/MainBody/ConditionalRederingInFunction/A-ConditionalRenderingTernary/CondRendF_Parent.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CondRendF_Parent = () => {
+  const [UserData, setUserData] = useState({});
   return <div></div>;
 };
 
