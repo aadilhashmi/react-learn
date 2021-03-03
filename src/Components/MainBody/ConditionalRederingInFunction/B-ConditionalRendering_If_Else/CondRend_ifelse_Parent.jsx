574c1b99c70ec27d222e1e53b5558d671c59ebeb
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-03 19:48:26 +0530

    hiiiiiiiiiiiii

---
+++ src/Components/MainBody/ConditionalRederingInFunction/B-ConditionalRendering_If_Else/CondRend_ifelse_Parent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CondRend_ifelse_Parent = () => {
+  return <div></div>;
+};
+
+export default CondRend_ifelse_Parent;
---
+++ src/Components/MainBody/ConditionalRederingInFunction/B-ConditionalRendering_If_Else/CondRend_ifesle_Child.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CondRend_ifesle_Child = () => {
+  return <div></div>;
+};
+
+export default CondRend_ifesle_Child;
