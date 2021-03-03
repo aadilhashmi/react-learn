d71d8ddf0fcbbb2f5f5b07177d239ae44db5f18c
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-03 12:35:54 +0530

    hiiiiiiiii

--- src/Components/MainBody/ConditionalRederingInFunction/A-ConditionalRenderingTernary/CondRendF_Parent.jsx
+++ src/Components/MainBody/ConditionalRederingInFunction/A-ConditionalRenderingTernary/CondRendF_Parent.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 
 const CondRendF_Parent = () => {
-  const [UserData, setUserData] = useState({});
+  const [UserData, setUserData] = useState({
+    user: [
+      { name: "Salman", age: 23 },
+      { name: "rizwan", age: 24 },
+      { name: "aamer", age: 20 },
+    ],
+  });
   return <div></div>;
 };
 
