5e1b28a210a81b648ecb40b8275f089452a33f92
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-04 16:42:13 +0530

    import radium

--- src/Components/MainBody/ConditionalRederingInFunction/Radium/RadiumParent.jsx
+++ src/Components/MainBody/ConditionalRederingInFunction/Radium/RadiumParent.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import Radium from "radium";
 
 const RadiumParent = () => {
+  const styleButton = {
+    backgroundColor: "Aquamarine",
+    color: "black",
+    boxShadow: "5px 10px 10px 0 black",
+  };
   return (
     <div>
       <h1>hiiiiiiiiiiiiiii</h1>
       <p>this is radium practice</p>
       <i>first install radium :=== npm i radium --save</i>
       <br />
-      <button>Radium</button>
+      <button style={styleButton}>Radium</button>
     </div>
   );
 };
 
-export default RadiumParent;
+export default RadiumParent(RadiumParent);
