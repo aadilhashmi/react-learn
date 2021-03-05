5d5c0c955156804c5236d9b04a001636899c6918
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-05 16:40:37 +0530

    hiiiiiiiiiiiii

--- src/Components/MainBody/ConditionalRederingInFunction/Styled-Component/StyledCompParent.jsx
+++ src/Components/MainBody/ConditionalRederingInFunction/Styled-Component/StyledCompParent.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledH1 = styled.h1`
-  background-color: red;
-  color: white;
+  background-color: aquamarine;
+  color: black;
+  widht: 200px;
 `;
 const StyledCompParent = () => {
   return (
