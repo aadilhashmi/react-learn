3926d3f7154a3a5f8c0bd43f6ce6835c5749363a
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-05 16:54:42 +0530

    hiiiiiiiiii

--- src/Components/MainBody/ConditionalRederingInFunction/Styled-Component/StyledCompParent.jsx
+++ src/Components/MainBody/ConditionalRederingInFunction/Styled-Component/StyledCompParent.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledH1 = styled.h1`
-  background-color: aquamarine;
-  color: black;
-  widht: 200px;
+  background-color: blue;
+  color: white;
+  width: 50%;
+  margin-left: 20%;
+  text-align: center;
+  border-radius: 10px;
 `;
 const StyledCompParent = () => {
   return (
