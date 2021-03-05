27820465599eb6e491eb324125af22e2efd5c42d
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-05 16:32:37 +0530

    make styled component

--- src/Components/MainBody/ConditionalRederingInFunction/Styled-Component/StyledCompParent.jsx
+++ src/Components/MainBody/ConditionalRederingInFunction/Styled-Component/StyledCompParent.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
+const StyledH1 = styled.h1`
+  background-color: red;
+  color: white;
+`;
 const StyledCompParent = () => {
-  return <div></div>;
+  return (
+    <div>
+      <StyledH1>PICKUPBIZZ</StyledH1>
+    </div>
+  );
 };
 
 export default StyledCompParent;
