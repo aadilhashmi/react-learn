d16425da2db4b9ff0b541353400873b353f78492
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-18 15:25:33 +0530

    hiiiiii

--- src/Components/Placement/PlacementStatstics/PlacementStats.jsx
+++ src/Components/Placement/PlacementStatstics/PlacementStats.jsx
@@ -4,7 +4,7 @@ import { Table } from "react-bootstrap";
 const PlacementStats = () => {
   return (
     <div>
-      <Table striped bordered hover>
+      <Table className="tab">
         <thead>
           <tr>
             <th>#</th>
