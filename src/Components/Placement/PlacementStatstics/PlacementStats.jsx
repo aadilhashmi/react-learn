57a34e67d61bc9bffefc6e974fc5b88f3a0aa89e
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-18 16:11:54 +0530

    hiiiiiiiiiiiiii

--- src/Components/Placement/PlacementStatstics/PlacementStats.jsx
+++ src/Components/Placement/PlacementStatstics/PlacementStats.jsx
@@ -4,80 +4,82 @@ import "./PlacementStats.css";
 
 const PlacementStats = () => {
   return (
-    <div>
-      <Table className="tab">
-        <thead className="head">
-          <tr>
-            <th>SrNo.</th>
-            <th>Students Name</th>
-            <th>Companey</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Jacob</td>
-            <td>twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>Jacob</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>Jacob</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>Jacob</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+    <React.Fragment>
+      <div>
+        <Table className="tab">
+          <thead className="head">
+            <tr>
+              <th>SrNo.</th>
+              <th>Students Name</th>
+              <th>Companey</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Jacob</td>
+              <td>twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>Jacob</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>Jacob</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>Jacob</td>
+              <td>@twitter</td>
+              <td>@fat</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </React.Fragment>
   );
 };
 
