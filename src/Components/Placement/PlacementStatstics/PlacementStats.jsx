05fa0023e5d8db986e355edefcc4ec27ce85750a
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-19 14:50:30 +0530

    hiiiii

--- src/Components/Placement/PlacementStatstics/PlacementStats.jsx
+++ src/Components/Placement/PlacementStatstics/PlacementStats.jsx
@@ -20,61 +20,61 @@ const PlacementStats = () => {
               <td>1</td>
               <td>Mark</td>
               <td>Otto</td>
-              <td>@mdo</td>
+              <td>2014</td>
             </tr>
             <tr>
               <td>2</td>
               <td>Jacob</td>
               <td>Thornton</td>
-              <td>@fat</td>
+              <td>2015</td>
             </tr>
             <tr>
               <td>3</td>
               <td>Jacob</td>
               <td>twitter</td>
-              <td>@fat</td>
+              <td>2016</td>
             </tr>
             <tr>
               <td>4</td>
               <td>Larry the Bird</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2017</td>
             </tr>
             <tr>
               <td>5</td>
               <td>Larry the Bird</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2018</td>
             </tr>
             <tr>
               <td>6</td>
               <td>Jacob</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2019</td>
             </tr>
             <tr>
               <td>7</td>
               <td>Larry the Bird</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2020</td>
             </tr>
             <tr>
               <td>8</td>
               <td>Larry the Bird</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2018</td>
             </tr>
             <tr>
               <td>9</td>
               <td>Jacob</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2019</td>
             </tr>
             <tr>
               <td>10</td>
               <td>Jacob</td>
               <td>@twitter</td>
-              <td>@fat</td>
+              <td>2018</td>
             </tr>
           </tbody>
         </Table>
