f5072bd15cdeca9d8d70610a9b23edb966aee497
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-06-06 21:48:52 +0530

    complete dark mode

--- src/Components/DarkThemeToggle/DarkThemeToggle.jsx
+++ src/Components/DarkThemeToggle/DarkThemeToggle.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Switch from "@material-ui/core/Switch";
+import Paper from "@material-ui/core/Paper";
 
 export const DarkThemeToggle = () => {
   const [darkMode, setDarkMode] = useState(false);
-
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
   return (
-    <div>
-      <p>DARK MODE THEME</p>
-      <Switch
-        checked={darkMode}
-        onChange={() => setDarkMode(!darkMode)}
-        color="primary"
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <Switch
+          checked={darkMode}
+          onChange={() => setDarkMode(!darkMode)}
+          name="checkedB"
+          color="primary"
+          label="SWITCH FOR DARK MODE"
+        />
+      </Paper>
+    </ThemeProvider>
   );
 };
