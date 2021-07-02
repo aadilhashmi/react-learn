f7959c26d5c8b63f251e8a25981b4d553f870cc4
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-07-02 15:02:26 +0530

    hiiiiii

--- src/Components/HeaderSection/Interview.jsx
+++ src/Components/HeaderSection/Interview.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import { InterviewChild } from "./InterviewChild";
 export const Interview = () => {
+  const [count, setCount] = useState();
+
   return (
     <>
       <div>
